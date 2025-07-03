664f059cb201191c7a4dad82b0fbbc071631b407
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-02 18:27:17 -0600

    connect database to user model

--- app/v1/model/user_model.py
+++ app/v1/model/user_model.py
@@ -1,7 +1,20 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
 
 class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True )
+    __tablename__ = "users"
+    uid : uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4,
+        )
+    )
     username: str
     password: str
     email: str
+
+def __repr__(self):
+    return f"<User {self.username}>"
--- app/v1/utils/db.py
+++ app/v1/utils/db.py
@@ -1,6 +1,7 @@
-from sqlmodel import create_engine, text
+from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from app.v1.utils.settings import Config
+from app.v1.model.user_model import User
 
 
 # Asynchronous database
@@ -12,6 +13,4 @@ engine = AsyncEngine(
 
 async def init_db():
     async with engine.begin() as conn:
-        statement = text("SELECT 'hello';")
-        result = await conn.execute(statement)
-        print(result.all())
+        await conn.run_sync(SQLModel.metadata.create_all)
