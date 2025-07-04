2eb0f5f89a0bdba2e0a6795c3b2f3b2451ae6c2a
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-04 15:58:01 -0600

    add http status

--- app/v1/router/user_router.py
+++ app/v1/router/user_router.py
@@ -7,10 +7,10 @@ from app.v1.schema.user_schema import UserCreate, UserUpdate, UserLogin, UserRea
 user_router = APIRouter()
 Us = UserService()
 
-@user_router.post("/register", response_model=UserRead)
+@user_router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserRead)
 async def register(user_data: UserCreate, session: AsyncSession = Depends(get_session)):
-    return await Us.register_user(user_data, session)
-
+    user_create = await Us.register_user(user_data, session)
+    return user_create
 @user_router.post("/login", response_model=UserRead)
 async def login(credentials: UserLogin, session: AsyncSession = Depends(get_session)):
     return await Us.login_user(credentials, session)
