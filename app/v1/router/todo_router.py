8bff110e3f8477a5dd3c33c9201cbe3757df9b6f
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-04 14:04:16 -0600

    router todolist

---
+++ app/v1/router/todo_router.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, status, Depends, HTTPException
+from app.v1.schema.todos_schema import TodoRead, TodoUpdate, TodoCreate
+from app.v1.service.todo_services import TodoService
+from app.v1.utils.db import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import List
+from uuid import UUID
+
+todo_router = APIRouter()
+ts = TodoService()
+
+# Admins
+@todo_router.get("/admin/todos", response_model=List[TodoRead])
+async def get_todos(session: AsyncSession = Depends(get_session)):
+    todos = await ts.get_all_todos(session)
+    return todos
+
+# Users
+@todo_router.get("/users/todos/{user_uid}", response_model=List[TodoRead])
+async def get_user_todos(user_uid: UUID, session: AsyncSession = Depends(get_session)):
+    todo = await ts.get_user_todos(user_uid ,session )
+    return todo
+
+@todo_router.get("/todos/{todo_id}", response_model=List[TodoRead])
+async def get_single_todo(todo_id: UUID, session: AsyncSession = Depends(get_session)):
+    todo = await ts.get_todo(todo_id, session)
+    return todo
+
+@todo_router.post("/todos", status_code=status.HTTP_201_CREATED, response_model=TodoRead)
+async def create_todo(todo_data: TodoCreate, session: AsyncSession = Depends(get_session)):
+    new_todo = await ts.create_todo(todo_data, session)
+    return new_todo
+
+@todo_router.patch("/todos/{todo_id}", response_model=TodoRead)
+async def update_todo(todo_id: UUID, update_data:TodoUpdate, session: AsyncSession = Depends(get_session)):
+    upd_todo = await ts.update_todo(todo_id, update_data, session)
+
+    if upd_todo is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return upd_todo
+
+@todo_router.delete("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(todo_id: UUID, session: AsyncSession = Depends(get_session)):
+    todo_delete = await ts.delete_todo(todo_id, session)
+
+    if todo_delete is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return todo_delete
