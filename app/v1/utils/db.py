5268f8793db20a31f9ee62aaca9368f4175de4dc
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-04 14:04:01 -0600

    add get_session async

--- app/v1/utils/db.py
+++ app/v1/utils/db.py
@@ -1,17 +1,28 @@
-from sqlmodel import create_engine, text, SQLModel
+
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import text, SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
 from app.v1.utils.settings import Config
+from sqlalchemy.orm import sessionmaker
 from app.v1.model.user_model import User
 from app.v1.model.todo_model import Todo
 
-
 # Asynchronous database
-engine = AsyncEngine(
-    create_engine(
+async_engine: AsyncEngine = create_async_engine(
     url=Config.DATABASE_URL,
     echo=True
-))
+)
 
-async def init_db():
-    async with engine.begin() as conn:
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    Session =  sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
