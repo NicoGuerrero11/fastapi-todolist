f775c68a29aef04806906c64d2b9879b57ebf6bb
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-04 15:57:46 -0600

    add user routes to the server

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from datetime import datetime
 from app.v1.utils.db import init_db
-
-
+from app.v1.router.user_router import user_router
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -14,3 +13,4 @@ async def life_span(app: FastAPI):
 
 
 app = FastAPI(lifespan=life_span)
+app.include_router(user_router, prefix="/api/users")
