b3704f334f2a2b69aa97082b57f4638282c729f9
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-08 20:03:57 -0600

    🔐 Add refresh and logout endpoints using token bearer dependencies with Redis blacklist

--- app/v1/router/user_router.py
+++ app/v1/router/user_router.py
@@ -3,9 +3,12 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from app.v1.utils.db import get_session
 from app.v1.service.user_service import UserService
 from app.v1.schema.user_schema import UserCreate, UserLogin, UserRead, TokenResponse
+from app.v1.dependencies.auth import RefreshTokenBearer, AccessTokenBearer
+
 
 user_router = APIRouter()
 Us = UserService()
+refresh_token = RefreshTokenBearer()
 
 @user_router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserRead)
 async def register(user_data: UserCreate, session: AsyncSession = Depends(get_session)):
@@ -15,4 +18,10 @@ async def register(user_data: UserCreate, session: AsyncSession = Depends(get_se
 async def login(credentials: UserLogin, session: AsyncSession = Depends(get_session)):
     return await Us.login_user(credentials, session)
 
+@user_router.get("/refresh", response_model=TokenResponse)
+async def refresh(user_details: dict = Depends(refresh_token), session: AsyncSession = Depends(get_session)):
+    return await Us.refresh_user_token(user_details, session)
 
+@user_router.get("/logout")
+async def revoke_token(user_details: dict = Depends(AccessTokenBearer())):
+    return await Us.revoke_token(user_details)
