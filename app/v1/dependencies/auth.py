73fb04c61caa286aaf434283b4f9c702bc4d3bc9
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-08 20:02:00 -0600

    ♻️ Add Redis token blacklist validation and refactor token type checks into subclasses for logout support

--- app/v1/dependencies/auth.py
+++ app/v1/dependencies/auth.py
@@ -2,6 +2,7 @@ from fastapi import Request, status
 from fastapi.security.http import HTTPBearer, HTTPAuthorizationCredentials
 from app.v1.scripts.token import decode_token
 from fastapi.exceptions import HTTPException
+from app.v1.utils.redis import token_in_blocklist
 
 class TokenBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
@@ -13,29 +14,46 @@ class TokenBearer(HTTPBearer):
         # creds.credentials -> token
         token = creds.credentials
 
+        token_data = decode_token(token)
+
         if not self.token_valid(token):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Invalid or expired token."
+                       "Resolution: Please get new Token."
             )
-        token_data = decode_token(token)
-
-        if token_data is None:
+        if await token_in_blocklist(token_data['jti']):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="Invalid or expired token."
+                detail="error: This token is invalid or has been revoked,"
+                       "Resolution: Please get new Token."
             )
 
-        if token_data.get("refresh_token"):
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token."
-            )
+        self.verify_token_data(token_data)
+
         return token_data
 
 
     def token_valid(self, token: str) -> bool:
         token_data = decode_token(token)
 
-        return True if token_data is not None else False
+        return token_data is not None
 
+    def verify_token_data(self,token_data):
+        raise NotImplementedError("Please override this method in child classes")
+
+class AccessTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data.get("refresh_token"):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token."
+            )
+
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data.get("refresh_token"):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide a refresh token."
+            )
