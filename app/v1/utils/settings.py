b6aeedecc8da64202deaa6e21b0128c505387f15
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-08 20:02:23 -0600

    ➕ Add Redis host and port settings to support token blacklist via environment variables

--- app/v1/utils/settings.py
+++ app/v1/utils/settings.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     JWT_ALGORITHM: str
+    REDIS_HOST : str = "localhost"
+    REDIS_PORT: int = 6379
 
     model_config = SettingsConfigDict(
         env_file='.env',
