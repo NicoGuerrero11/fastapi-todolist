e767ce4e963287b3d96db9db0a879edb58daa2fd
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-10 21:58:53 -0600

    feat(user): add /me route with role-based access check
    - Added /me endpoint to return current user info.
    - Integrated get_current_user dependency.
    - Applied RoleCheck to restrict access to users with 'admin' or 'user' roles.

--- app/v1/router/user_router.py
+++ app/v1/router/user_router.py
@@ -3,12 +3,14 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from app.v1.utils.db import get_session
 from app.v1.service.user_service import UserService
 from app.v1.schema.user_schema import UserCreate, UserLogin, UserRead, TokenResponse
-from app.v1.dependencies.auth import RefreshTokenBearer, AccessTokenBearer
+from app.v1.dependencies.auth import RefreshTokenBearer, AccessTokenBearer, get_current_user
+from app.v1.dependencies.roles import RoleCheck
 
 
 user_router = APIRouter()
 Us = UserService()
 refresh_token = RefreshTokenBearer()
+role_check = RoleCheck(['admin', 'user'])
 
 @user_router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserRead)
 async def register(user_data: UserCreate, session: AsyncSession = Depends(get_session)):
@@ -21,6 +23,10 @@ async def login(credentials: UserLogin, session: AsyncSession = Depends(get_sess
 @user_router.get("/refresh", response_model=TokenResponse)
 async def refresh(user_details: dict = Depends(refresh_token), session: AsyncSession = Depends(get_session)):
     return await Us.refresh_user_token(user_details, session)
+
+@user_router.get("/me")
+async def get_current_user(user=Depends(get_current_user), _:bool = Depends(role_check)):
+    return user
 
 @user_router.get("/logout")
 async def revoke_token(user_details: dict = Depends(AccessTokenBearer())):
