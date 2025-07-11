67f9cd328e9d6180f3b91881b3e0897937c1e821
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-07-10 21:54:22 -0600

    feat(app): register admin routes and improve app metadata

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from datetime import datetime
+from app.v1.router.admin_router import admin_router
 from app.v1.utils.db import init_db
 from app.v1.router.user_router import user_router
 from app.v1.router.todo_router import todo_router
@@ -13,6 +14,16 @@ async def life_span(app: FastAPI):
     print(f"Server has been stop at {datetime.now()} ")
 
 
-app = FastAPI(lifespan=life_span)
+app = FastAPI(
+    title="TODO App",
+    description="A REST API for a todo list",
+    version="v1"
+)
+# Users routes
 app.include_router(user_router, prefix="/api/users")
+
+# Todos routes
 app.include_router(todo_router, prefix="/api")
+
+# Admins routes
+app.include_router(admin_router, prefix="/api/admin")
